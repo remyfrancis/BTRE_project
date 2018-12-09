5205f4705cc7aad10caa1766e531beb74bd94563
remyfrancis <remy.e.francis@gmail.com>
2018-12-09 15:03:12 -0400

    Removed email from production settings

--- btre/settings.py
+++ btre/settings.py
@@ -142,13 +142,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'remy.e.francis@gmail.com'
-EMAIL_HOST_PASSWORD = 'yadseut13'
-EMAIL_USE_TLS = True
-
 try:
     from .local_settings import *
 except ImportError:
